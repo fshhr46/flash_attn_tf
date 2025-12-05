738ee4a663a2ded85b78d9d0d18bdb5c5f7bb310
hhuang2 <hhuang2@snapchat.com>
2025-12-05 23:14:32 +0000

    fix

--- flash_attn_tf/__init__.py
+++ flash_attn_tf/__init__.py
@@ -1,16 +1,16 @@
 import os
 
-if os.environ.get("FLASH_ATTN_TF_USE_AS_DEPENDENCY") == "1":
-    from flash_attn_tf import flash_mha
-    __all__ = ["flash_mha"]
+# if os.environ.get("FLASH_ATTN_TF_USE_AS_DEPENDENCY") == "1":
+#     from flash_attn_tf import flash_mha
+#     __all__ = ["flash_mha"]
 
-else:
-    from flash_attn_tf import (
-        flash_mha,
-    )
-    from flash_attn_tf._version import __version__
+# else:
+#     from flash_attn_tf import (
+#         flash_mha,
+#     )
+#     from flash_attn_tf._version import __version__
 
-    __all__ = [
-        "flash_mha",
-        "__version__",
-    ]
+#     __all__ = [
+#         "flash_mha",
+#         "__version__",
+#     ]
--- flash_attn_tf/python/__init__.py
+++ flash_attn_tf/python/__init__.py
@@ -2,8 +2,8 @@
 __version__ = 'v0.1.0'
 
 
-from flash_attn_tf.python.op.flash_attn_ops import flash_mha
+# from flash_attn_tf.python.op.flash_attn_ops import flash_mha
 
-__all__ = [
-    "flash_mha",
-]
+# __all__ = [
+#     "flash_mha",
+# ]
