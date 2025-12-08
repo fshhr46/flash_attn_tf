c13c0acee4f2f11c07cb010de9db08c868973dab
hhuang2 <hhuang2@snapchat.com>
2025-12-08 09:12:51 +0000

    cleanup

--- flash_attn_tf/python/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from flash_attn_tf.python.ops.flash_attn_ops import flash_mha_fwd, flash_mha_bwd
-
