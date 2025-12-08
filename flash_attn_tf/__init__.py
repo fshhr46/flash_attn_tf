43bc7fbd74a4c1ed9a3746ff7073031d3e6b4f89
hhuang2 <hhuang2@snapchat.com>
2025-12-08 08:59:28 +0000

    fix

--- flash_attn_tf/__init__.py
+++ flash_attn_tf/__init__.py
@@ -2,6 +2,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from flash_attn_tf.python.ops.time_two_ops import time_two
 from flash_attn_tf.python.ops.flash_attn_ops import flash_mha_fwd, flash_mha_bwd
 
