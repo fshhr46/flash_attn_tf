9a2f78e7b1a17617da86100031cc7a4a157b3a45
hhuang2 <hhuang2@snapchat.com>
2025-12-12 01:32:15 +0000

    break into 2

--- flash_attn_tf/python/ops/flash_attn_ops.py
+++ flash_attn_tf/python/ops/flash_attn_ops.py
@@ -5,8 +5,10 @@ from __future__ import print_function
 from tensorflow.python.framework import load_library
 from tensorflow.python.platform import resource_loader
 
-flash_attn_ops = load_library.load_op_library(
-    resource_loader.get_path_to_datafile('_flash_attn_ops.so'))
+flash_attn_ops_fwd = load_library.load_op_library(
+    resource_loader.get_path_to_datafile('_flash_attn_ops_fwd.so'))
+flash_attn_ops_bwd = load_library.load_op_library(
+    resource_loader.get_path_to_datafile('_flash_attn_ops_bwd.so'))
 
-flash_mha_fwd = flash_attn_ops.flash_mha_fwd
-flash_mha_bwd = flash_attn_ops.flash_mha_bwd
+flash_mha_fwd = flash_attn_ops_fwd.flash_mha_fwd
+flash_mha_bwd = flash_attn_ops_bwd.flash_mha_bwd
